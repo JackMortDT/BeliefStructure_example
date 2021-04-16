dbe87eb561b804cd0a97fbe1909a36e4aa713b7e
Jack <le.sastre@ebc.edu.mx>
2021-04-15 22:43:51 -0500

    Adding dialyzer deps

--- lib/belief_struture/Hexify.ex
+++ lib/belief_struture/Hexify.ex
@@ -1,9 +1,11 @@
 defmodule BeliefStructure.Hexify do
   @moduledoc false
+  @spec name(String.t()) :: String.t()
   def name(package) do
     package(package)
   end
 
+  @spec package(String.t()) :: String.t()
   defp package(package) do
     package <> "_ex"
   end
--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule BeliefStructure.MixProject do
   def project do
     [
       app: :belief_structure,
+      dialyzer: [plt_add_deps: :app_tree],
       version: "0.1.0",
       elixir: "~> 1.12-rc",
       start_permanent: Mix.env() == :prod,
@@ -21,7 +22,8 @@ defmodule BeliefStructure.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.5.5", only: [:dev], runtime: false}
+      {:credo, "~> 1.5.5", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
